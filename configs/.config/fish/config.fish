8af20652c673409d9554692c09cf66ad69179bbd
Volodymyr Gamula <v.gamula@gmail.com>
2025-07-27 00:56:46 +0100

    Simplify config management, use GNU Stow for symlinks, Alacritty -> Ghostty, remove redundant deps

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -1,6 +1,5 @@
-set -x SHELL "fish"
-
 # Path
+set fish_greeting
 set -x PATH $PATH /opt/homebrew/bin
 set -x PATH $PATH $HOME/bin
 set -x PATH $PATH $HOME/.local/bin
@@ -8,24 +7,18 @@ set -x PATH $PATH $HOME/.cargo/bin
 set -x PATH $PATH /usr/local/bin
 set -x PATH $PATH /usr/local/sbin
 set -x PATH $PATH /opt/homebrew/opt/libpq/bin
-set -x JAVA_HOME $HOME/.sdkman/candidates/java/current
-
+set -x PATH $PATH /opt/homebrew/opt/openjdk/bin
+set -x JAVA_HOME /opt/homebrew/opt/openjdk
 # Fly.io
-set -x FLYCTL_INSTALL "/Users/vgamula/.fly"
+set -x FLYCTL_INSTALL "/Users/$USER/.fly"
 set -x PATH "$FLYCTL_INSTALL/bin" $PATH
-
-alias tmux 'tmux -2'
-
 # Signed commits
 set -x GPG_TTY (tty)
-
-# Starship
-starship init fish | source
-
 # opam configuration
 source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
-yes | fish_config theme save "Catppuccin Frappe"
-
 alias code zed
 alias python 'uv run python'
+
+# Starship
+starship init fish | source
--- install.sh
+++ install.sh
@@ -1,110 +1,52 @@
 #!/usr/bin/env bash
 
+echo "🚧"
+
 set -e
 
-install_packages() {
-    echo "Install packages..."
-    brew update
-    brew upgrade
+brew update
+brew upgrade
 
-    brew install git
+packages=(
+    git                  # vcs
+    fish                 # shell
+    zellij               # terminal multiplexer
+    starship             # shell prompt
+    fzf                  # fuzzy file search
+    neovim               # one more text editor
+    dust                 # du
+    ripgrep              # grep
+    bat                  # cat
+    eza                  # ls
+    fd                   # find
+    procs                # top
+    tokei                # lines of code
+    hyperfine            # benchmarking
+    oha                  # http benchmarking
+    uv                   # python versions and envs
+    inetutils            # telnet/ftp and others
+    gnupg                # signed stuff
+    pinentry-mac         # signed stuff
+    lazygit              # git tui
+    zellij               # terminal multiplexer
+    font-hack-nerd-font  # icons
+    stow                 # symlinks
+)
+brew install "${packages[@]}"
 
-    # Shell
-    brew install fish fisher
-    brew install starship
-    brew install zellij
+casks=(
+    ghostty
+    discord
+    raycast
+    spotify
+    zed
+    font-iosevka
+    font-iosevka-slab
+    font-iosevka-nerd-font
+)
+brew install --cask "${casks[@]}"
 
-    # brew install reattach-to-user-namespace
-    #if [[ ! $SHELL == *"fish"* ]];
-    #then
-    #    chsh -s $(which fish)
-    #fi
-    brew install fzf
-    yes | $(brew --prefix)/opt/fzf/install
+mkdir -p ~/.config
+stow configs
 
-    # Editor
-    brew install neovim
-
-    # Tools
-    brew install dust              # du
-    brew install ripgrep           # grep
-    brew install bat               # cat
-    brew install eza               # ls
-    brew install fd                # find
-    brew install procs             # top
-    brew install tokei             # lines of code
-    brew install hyperfine         # benchmarking
-    brew install oha               # http benchmarking
-    brew install uv                # python versions and envs
-    brew install inetutils         # telnet/ftp
-    brew install gnupg             # signed stuff
-    brew install pinentry-mac      # signed stuff
-    brew install lazygit           # git tui
-    brew install zellij            # terminal multiplexer
-
-    # TODO: extra into setup_shell?
-    # fisher install reitzig/sdkman-for-fis
-    # fisher install catppuccin/fish
-
-    echo "Install packages... Done"
-}
-
-install_extra_packages() {
-    # brew install --cask discord
-    # brew install --cask iina
-    # brew install --cask qbittorrent
-    brew install --cask raycast
-    # brew install --cask spotify
-    brew install --cask visual-studio-code
-    brew install --cask zed
-    # brew install --cask telegram
-    # brew install --cask vlc
-    # brew install --cask zoomus
-}
-
-install_fonts() {
-    brew install --cask font-iosevka
-    brew install --cask font-iosevka-slab
-}
-
-setup_links() {
-    echo "Setup links..."
-    rm -f $HOME/.functions
-    ln -sf $HOME/dotfiles/.functions $HOME/.functions
-
-    rm -rf $HOME/.config/alacritty
-    ln -sf $HOME/dotfiles/.config/alacritty $HOME/.config/alacritty
-
-    rm -f $HOME/.config/fish/config.fish
-    ln -sf $HOME/dotfiles/.config/fish/config.fish $HOME/.config/fish/config.fish
-
-    rm -f $HOME/.gitconfig
-    ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-
-    mkdir -p $HOME/.lein
-    ln -sf $HOME/dotfiles/.lein/profiles.clj $HOME/.lein/profiles.clj
-
-    mkdir -p $HOME/.gnupg
-    ln -sf $HOME/dotfiles/.gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
-
-    mkdir -p $HOME/config/zellij
-    rm -rf rm -f $HOME/.config/zellij/config.kdl
-    ln -sf $HOME/dotfiles/.config/zellij/config.kdl $HOME/.config/zellij/config.kdl
-
-    rm -rf rm -f $HOME/.config/starship.toml
-    ln -sf $HOME/dotfiles/.config/starship.toml $HOME/.config/starship.toml
-    echo "Setup links... Done"
-}
-
-main() {
-    export HOMEBREW_NO_AUTO_UPDATE=1
-
-    install_packages
-    # install_extra_packages
-    install_fonts
-    setup_links
-    export HOMEBREW_NO_AUTO_UPDATE=
-
-    echo "Yay, all good!"
-}
-main
+echo "✅"
