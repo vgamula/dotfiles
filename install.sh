c9b09edbfdf18639f1906381922f86a6e4c0c233
Volodymyr Gamula <v.gamula@gmail.com>
2025-07-28 00:05:39 +0100

    Remove redunant font + format

--- install.sh
+++ install.sh
@@ -8,41 +8,40 @@ brew update
 brew upgrade
 
 packages=(
-    git                  # vcs
-    fish                 # shell
-    zellij               # terminal multiplexer
-    starship             # shell prompt
-    fzf                  # fuzzy file search
-    neovim               # one more text editor
-    dust                 # du
-    ripgrep              # grep
-    bat                  # cat
-    eza                  # ls
-    fd                   # find
-    procs                # top
-    tokei                # lines of code
-    hyperfine            # benchmarking
-    oha                  # http benchmarking
-    uv                   # python versions and envs
-    inetutils            # telnet/ftp and others
-    gnupg                # signed stuff
-    pinentry-mac         # signed stuff
-    lazygit              # git tui
-    zellij               # terminal multiplexer
-    font-hack-nerd-font  # icons
-    stow                 # symlinks
+  git          # vcs
+  fish         # shell
+  zellij       # terminal multiplexer
+  starship     # shell prompt
+  fzf          # fuzzy file search
+  neovim       # one more text editor
+  dust         # du
+  ripgrep      # grep
+  bat          # cat
+  eza          # ls
+  fd           # find
+  procs        # top
+  tokei        # lines of code
+  hyperfine    # benchmarking
+  oha          # http benchmarking
+  uv           # python versions and envs
+  inetutils    # telnet/ftp and others
+  gnupg        # signed stuff
+  pinentry-mac # signed stuff
+  lazygit      # git tui
+  zellij       # terminal multiplexer
+  stow         # symlinks
 )
 brew install "${packages[@]}"
 
 casks=(
-    ghostty
-    discord
-    raycast
-    spotify
-    zed
-    font-iosevka
-    font-iosevka-slab
-    font-iosevka-nerd-font
+  ghostty
+  discord
+  raycast
+  spotify
+  zed
+  font-iosevka
+  font-iosevka-slab
+  font-iosevka-nerd-font
 )
 brew install --cask "${casks[@]}"
 
